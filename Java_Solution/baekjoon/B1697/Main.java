1bb575bdd5e1663a16e74eddb97a9aa4a0684245
LeeHeongyu <frtt0608@naver.com>
2019-08-24 16:12:14 +0900

    190824|백준 B1697 숨바꼭질 문제 풀이 중...

--- Java_Solution/baekjoon/B1697/Main.java
+++ Java_Solution/baekjoon/B1697/Main.java
@@ -5,16 +5,32 @@ import java.util.*;
 public class Main {
   static int N;
   static int K;
-  static int time;
+  static int t;
+  static int[] v;
 
-  static void BFS(int w, int t) {
-    if(t>0 && w==N) return;
-    if(w < N) return;
-    if(w >= K) return;
-    if(w-1 == N || w+1 == N) return;
-    BFS(w-1, time+1);
-    BFS(2*w, time+1);
-    BFS(w+1, time+1);
+  static void BFS_odd(int w) {
+    LinkedList<Integer> qu = new LinkedList();
+    int time = 0;
+    qu.add(w);
+    while(!qu.isEmpty()) {
+
+    }
+  }
+
+  static void BFS_even(int w) {
+    LinkedList<Integer> qu = new LinkedList();
+    int time = 0;
+    qu.add(w);
+    while(!qu.isEmpty()) {
+      int p = qu.poll();
+      if(p==N) {
+        System.out.println("time: " + time);
+        return;
+      }
+      if(N <= p/2) {
+        
+      }
+    }
   }
 
   static public void main(String[] args) throws IOException {
@@ -23,8 +39,10 @@ public class Main {
 
     N = in.nextInt();
     K = in.nextInt();
-    time = 0;
-    BFS(N, time);
-    System.out.println(time);
+    t=0;
+    if(K%2==0)
+      BFS_even(K);
+    else
+      BFS_odd(K);
   }
 }
