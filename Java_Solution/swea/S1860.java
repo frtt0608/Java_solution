ea60b0bea3bfbb5c6cee5175ad3bef785d68b55d
LeeHeongyu <frtt0608@naver.com>
2019-10-06 23:25:02 +0900

    191006|SWEA S1860 풀이 중

---
+++ Java_Solution/swea/S1860.java
@@ -0,0 +1,16 @@
+// 진기의 최고급 붕어빵
+import java.io.*;
+import java.util.*;
+
+public class S1860 {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        Scanner sc = new Scanner(System.in);
+    
+        int T = sc.nextInt();
+        for(int tc=1; tc<=T; tc++) {
+            
+        }
+        sc.close();
+    }
+}
