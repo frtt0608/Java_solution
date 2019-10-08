911852b44a95564043e4c4f1cad941c11d786833
LeeHeongyu <frtt0608@naver.com>
2019-10-09 00:04:20 +0900

    191009|SWEA S3307 문제풀이

--- Java_Solution/swea/S3307.java
+++ Java_Solution/swea/S3307.java
@@ -1,9 +1,10 @@
 // 최장 증가 부분 수열
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Scanner;
-import java.io.*;
 
 public class S3307 {
-  static int visit[], N, table[], res;
+  static int count[], N, table[], res;
 
   public static void main(String[] args) throws IOException {
     System.setIn(new FileInputStream("input.txt"));
@@ -13,14 +14,21 @@ public class S3307 {
     for(int tc=1;tc<=T;tc++) {
       N = sc.nextInt();
       table = new int[N];
-      visit = new int[N];
+      count = new int[N];
       res = 0;
       for(int n=0; n<N; n++) {table[n] = sc.nextInt();}
       for(int i=0; i<N; i++) {
-        
+        if(N-i<res) break;
+        for(int j=i+1; j<N; j++) {
+          if(table[i] > table[j]) continue;
+          if(count[i]+1 <= count[j]) continue;
+          count[j] = count[i] + 1;
+          if(res<count[j]) res=count[j];
+        }
       }
-      
+      res+=1;
       System.out.println("#"+tc+" "+res);
     }
+    sc.close();
   }
 }
