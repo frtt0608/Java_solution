12c53cdb100b0019893c13d3979de244ea6176a1
LeeHeongyu <frtt0608@naver.com>
2019-10-13 23:41:38 +0900

    191013|SWEA S5648 풀이 중

--- Java_Solution/swea/A+ 대비/S5648.java
+++ Java_Solution/swea/A+ 대비/S5648.java
@@ -1,11 +1,28 @@
 // 원자 소멸 시뮬레이션
+// #1 24
+// #2 0
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.LinkedList;
+
+class Atom {
+    int x,y,dir,e;
+    Boolean live;
+
+    Atom(int x, int y, int dir, int e, Boolean live) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+        this.e = e;
+        this.live = live;
+    }
+}
 
 public class S5648 {
-    static int N, map[][];
-    static int dr[]={}, dc[]={};
+    static int N;
+    static int dx[]={0,0,-1,1}, dy[]={1,-1,0,0};
+    static LinkedList<Atom> map;
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         Scanner sc = new Scanner(System.in);
@@ -13,12 +30,15 @@ public class S5648 {
         int T = sc.nextInt();
         for(int tc=1; tc<=T; tc++) {
             N = sc.nextInt();
-            map = new int[N][2];
+            map = new LinkedList<Atom>();
             for(int i=0; i<N; i++) {
-                int x=sc.nextInt();
-                int y=sc.nextInt();
-                
+                int x = sc.nextInt(); // x
+                int y =sc.nextInt(); // y
+                int dir = sc.nextInt(); // 방향
+                int e = sc.nextInt(); // 에너지
+                map.add(new Atom(x,y,dir,e,true));
             }
+
         }
     }
 }
