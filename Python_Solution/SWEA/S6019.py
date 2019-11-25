764b6a22526c4032a20562d28b9ca05a1fb7e283
Lee heon gyu <frtt0608@naver.com>
2019-11-25 18:49:07 +0900

    191125|SWEA S6019 풀이

--- Python_Solution/SWEA/S6019.py
+++ Python_Solution/SWEA/S6019.py
@@ -4,16 +4,11 @@ sys.stdin = open("input.txt")
 T=int(input())
 
 for tc in range(1,T+1):
-  data = list(map(int, input().split()))
+  data = list(map(float, input().split()))
   D, A, B, F = data[0], data[1], data[2], data[3]
-  t=0
-  res=0
-  move_train = 0
-  move_fly = 0
-  flag = True
-  while D>0:
-    t+=1
-    move_train = t*(A+B)
-    if flag:
-      move_fly = t*(F+B)
-      
+  
+  res = 0.0
+  time = D/(A+B)
+  res = F*time
+  
+  print('#%d %.6f' %(tc, res))
