380ca1c3e202280879db947f425c74d5ec5e350b
Lee heon gyu <frtt0608@naver.com>
2019-12-02 11:54:37 +0900

    191202|SWEA S6057 풀이

--- Python_Solution/SWEA/S6057.py
+++ Python_Solution/SWEA/S6057.py
@@ -2,27 +2,11 @@
 import sys
 sys.stdin = open("input.txt")
 
-def DFS(init, index, cnt):
-  global res
-  if cnt==3:
-    if init==index:
-      res += 1
-    return
-
-  for id in range(1,N+1):
-    if v[index][id]==1: continue
-    if table[index][id]==1:
-      v[index][id]=1
-      v[id][index]=1
-      DFS(init, index, cnt+1)
-  
-
 T=int(input())
 for tc in range(1,T+1):
   NM=list(map(int, input().split()))
   N,M = NM[0],NM[1]
   table = [[0 for i in range(N+1)] for j in range(N+1)]
-  v = [[0 for i in range(N+1)] for j in range(N+1)]
   res = 0
 
   for n in range(M):
@@ -31,8 +15,12 @@ for tc in range(1,T+1):
     table[xy[1]][xy[0]] = 1
 
   for i in range(1,N+1):
-    for j in range(1,N+1):
+    for j in range(i,N+1):
       if table[i][j]==1:
-        DFS(i,i,0)
+        for k in range(1,N+1):
+          if table[i][k]==1 and table[j][k]==1:
+            res += 1
+
+  res /= 3
 
   print('#%d %d' %(tc, res))
