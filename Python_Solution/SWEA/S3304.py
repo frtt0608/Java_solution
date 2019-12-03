a01b304a6b788ce65729cf721176eb1f03da7b06
Lee heon gyu <frtt0608@naver.com>
2019-12-03 16:31:47 +0900

    191203|SWEA S3304 문제 풀이

--- Python_Solution/SWEA/S3304.py
+++ Python_Solution/SWEA/S3304.py
@@ -1,29 +1,27 @@
-# 최장 공통 부분 수열
+# 최장 공통 부분 수열 LCS
 import sys
 sys.stdin = open("input.txt")
-
-def find(index, data):
-  global data2
-  for i in range(index, len(data2)):
-    if data==data2[i]:
-      return i, True
-  return index, False
 
 T=int(input())
 for tc in range(1, T+1):
   data = list(map(str, input().split()))
   data1, data2 = data[0], data[1]
-  
-  max_res = 0
-  for i in range(len(data1)-1):
-    temp=0
-    ret=0
-    for j in range(i, len(data1)):
-      ret, flag = find(ret+1,data1[j])
-      if flag:
-        temp+=1
+  data1 = "0" + data1 # 0acaykp
+  data2 = "0" + data2 # 0capcak
+  table = [[0 for i in range(len(data1))] for j in range(len(data2))]
 
-    if max_res < temp:
-      max_res = temp
+  for i in range(1, len(data2)):
+    for j in range(1, len(data1)):
+      if data1[j]==data2[i]:
+        table[i][j] = table[i-1][j] + 1
 
-  print('#%d %d' %(tc, max_res))
+      else:
+        if table[i-1][j] > table[i][j-1]:
+          table[i][j] = table[i-1][j]
+        else:
+          table[i][j] = table[i][j-1]
+
+  for i in table:
+    print(i)
+
+  print('#%d %d' %(tc, table[len(data2)-1][len(data1)-1]))
