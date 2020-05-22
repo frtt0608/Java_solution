6b88e8c74b46761fe9c5d5db076422fbc37357a5
Heon_9u <frtt0608@naver.com>
2020-05-22 16:29:45 +0900

    200522|백준|반도체설계 복습_LIS

---
+++ Java_Solution/baekjoon/GreedyAlgo/반도체설계복습/Main.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Main
+ */
+public class Main {
+    static int N, port[];
+
+    static int LIS() {
+        int LIS_len = 1;
+        int connect[] = new int[N];
+        int idx = 0;
+        connect[0] = port[0];
+
+        for(int i=0; i<N; i++) {
+            if(connect[0] > port[0]) {
+                connect[0] = port[0];
+            } else if(connect[LIS_len-1] < port[i]) {
+                connect[LIS_len] = port[i];
+                LIS_len += 1;
+            } else {
+                idx = BinarySearch(LIS_len, connect, port[i]);
+                connect[idx] = port[i];
+            }
+        }
+
+        return LIS_len;
+    }
+
+    static int BinarySearch(int max, int connect[], int val) {
+        int min = 0;
+        int mid = 0;
+
+        while(max >= min) {
+            mid = (max+min)/2;
+
+            if(connect[mid] < val) {
+                min = mid+1;
+            } else if(connect[mid] > val) {
+                max = mid-1;
+            } else {
+                return mid;
+            }
+        }
+
+        return min;
+    }
+
+    static public void main(String args[]) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+        port = new int[N];
+        String input[] = br.readLine().split(" ");
+        for(int i=0; i<N; i++) {
+            port[i] = Integer.parseInt(input[i]);
+        }
+
+        System.out.println(LIS());
+    }
+}
---
+++ Java_Solution/baekjoon/GreedyAlgo/한줄로서기/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * Main
+ */
+public class Main {
+
+    static public void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+    }
+}
