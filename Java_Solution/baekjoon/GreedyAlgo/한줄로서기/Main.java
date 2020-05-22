dea8f562c54f94d10114ef4a33219a395969e66b
Heon_9u <frtt0608@naver.com>
2020-05-22 17:33:18 +0900

    200522|백준|한줄로서기|입력값이 나의 왼쪽에 있는 사람 중 키가 큰 사람 수를 나타내므로 키가 큰 것부터 탐색_입력값이 List의 index가 된다.

--- Java_Solution/baekjoon/GreedyAlgo/한줄로서기/Main.java
+++ Java_Solution/baekjoon/GreedyAlgo/한줄로서기/Main.java
@@ -8,5 +8,24 @@ public class Main {
 
     static public void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String input[] = br.readLine().split(" ");
+        int leftCnt[] = new int[N];
+
+        for(int i=0; i<N; i++) {
+            leftCnt[i] = Integer.parseInt(input[i]);
+        }
+
+        LinkedList<Integer> height = new LinkedList<>();
+        for(int i=N-1; i>=0; i--) {
+            height.add(leftCnt[i], i);
+        }
+
+        for(int i=0; i<N; i++) {
+            System.out.print(height.get(i)+1);
+            if(i < N-1) System.out.print(" ");
+        }
     }
 }
