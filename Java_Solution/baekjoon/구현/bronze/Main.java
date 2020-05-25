a61d6b7fea1215b18862a3c35a8b2ebf60d1b592
Heon_9u <frtt0608@naver.com>
2020-05-25 15:00:05 +0900

    200525|백준|세로읽기|입력값 전처리하기

--- Java_Solution/baekjoon/구현/bronze/Main.java
+++ Java_Solution/baekjoon/구현/bronze/Main.java
@@ -9,21 +9,22 @@ public class Main {
     static public void main(String args[]) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int res = 0;
-        int minPrice = 2001;
-        int price = 0;
+        int length = 0;
+        char chrArr[][] = new char[5][];
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<5; i++) {
+            chrArr[i] = br.readLine().toCharArray();
+            length = Math.max(length, chrArr[i].length);
+        }
 
-        for(int i=0; i<5; i++ ) {
-            price = Integer.parseInt(br.readLine());
-            minPrice = Math.min(minPrice, price);
-
-            if(i==2) {
-                res += minPrice;
-                minPrice = 2001;
+        for(int i=0; i<length; i++) {
+            for(int j=0; j<5; j++) {
+                if(chrArr[j].length <= i) continue;
+                else if(chrArr[j][i] == ' ') continue;
+                sb.append(chrArr[j][i]);
             }
         }
-        res += minPrice - 50;
-        System.out.println(res);
+
+        System.out.println(sb);
     }
 }
