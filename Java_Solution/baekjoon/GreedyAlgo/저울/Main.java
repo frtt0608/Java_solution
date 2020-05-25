db6bf6800514b8aaafd8a8f555875100fa479869
Heon_9u <frtt0608@naver.com>
2020-05-25 13:15:07 +0900

    200525|백준|저울|최소, 최대의 케이스를 고려해보며 알고리즘 구현하기

--- Java_Solution/baekjoon/GreedyAlgo/저울/Main.java
+++ Java_Solution/baekjoon/GreedyAlgo/저울/Main.java
@@ -6,16 +6,6 @@ import java.util.*;
  */
 public class Main {
     static int N, weights[];
-    static LinkedList<Integer> numList;
-
-    static void perm(int idx, int val) {
-        if(idx==N) {
-            numList.add(val);
-        }
-
-        perm(idx+1, val+weights[idx]);
-        perm(idx+1, val);
-    }
 
     static public void main(String args[]) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
@@ -23,26 +13,22 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         String input[] = br.readLine().split(" ");
         weights = new int[N];
-        int measure = 0;
-        numList = new LinkedList<>();
         
-
         for(int i=0; i<N; i++) {
             weights[i] = Integer.parseInt(input[i]);
         }
         
         Arrays.sort(weights); // 1 1 2 3 6 7 30
+        int measure = 0;
 
-        
         for(int i=0; i<N; i++) {
-            // System.out.println(measure + ", " + weights[i]);
             if(weights[i] <= measure+1) {
                 measure += weights[i];
             } else {
-                measure += 1;
-                break;
+                break;  
             }
         }
+        measure += 1;
 
         System.out.println(measure);
     }
