f05dfd19f37028e368d04177370c789aa62517ea
Heon_9u <frtt0608@naver.com>
2020-05-25 14:36:44 +0900

    200525|백준|알람시계|시간,분 맞추기

--- Java_Solution/baekjoon/구현/bronze/Main.java
+++ Java_Solution/baekjoon/구현/bronze/Main.java
@@ -9,29 +9,20 @@ public class Main {
     static public void main(String args[]) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int mon[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String days[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
-        // 1월 1일은 월요일
+        
         String input[] = br.readLine().split(" ");
-        int x = Integer.parseInt(input[0]);
-        int y = Integer.parseInt(input[1]);
+        int hour = Integer.parseInt(input[0]);
+        int minute = Integer.parseInt(input[1]);
 
-        int m = 1;
-        int d = 1;
-        String day = "MON";
-        int idx = d;
-
-        while(x != m || y != d) {
-            d++;
-            idx += 1;
-            if(mon[m] < d) {
-                m += 1;
-                d = 1;
+        minute -= 45;
+        if(minute < 0) {
+            hour -= 1;
+            minute += 60;
+            if(hour < 0) {
+                hour = 23;
             }
-            day = days[idx%days.length];
-            // System.out.println(m+" / "+d + ", day:" + day);
         }
 
-        System.out.println(day);
+        System.out.println(hour+" "+minute);
     }
 }
