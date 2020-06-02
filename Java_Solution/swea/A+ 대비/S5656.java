9a4a60460788deac5d5d546ec692c3b9e7934dc9
Heon_9u <frtt0608@naver.com>
2020-06-02 20:39:18 +0900

    200602|swea|벽돌깨기|copyMap활용하고 시간단축하기

--- Java_Solution/swea/A+ 대비/S5656.java
+++ Java_Solution/swea/A+ 대비/S5656.java
@@ -6,6 +6,7 @@ import java.io.*;
  */
 public class S5656 {
     static int N, W, H;
+    static int minBrick;
 
     static class Node {
         int x;
@@ -17,67 +18,105 @@ public class S5656 {
         }
     }
 
-    static int[][] downTobrick(int map[][]) {
+    static void downTobrick(int copy_map[][]) {
         int zeroIdx = H-1;
-        int temp = 0;
+
         for(int j=0; j<W; j++) {
             zeroIdx = H-1;
             for(int i=H-1; i>=0; i--) {
-                if(map[i][j] > 0) {
-                    temp = map[i][j];
-                    map[i][j] = 0;
-                    map[zeroIdx][j] = temp;
-                    zeroIdx++;
+                if(copy_map[i][j] > 0) {
+                    int temp = copy_map[i][j];
+                    copy_map[i][j] = 0;
+                    copy_map[zeroIdx][j] = temp;
+                    zeroIdx--;
                 }
             }
         }
-        
-        return map;
     }
 
-    static void startBrickGame(int cnt) {
-        if(cnt==N) {
-            
-            return;
-        }
-
-        for(int i=0; i<N; i++) {
-
-        }
-    }
-
-    static void findBrick(int idx, int map[][]) {
-        for(int i=0; i<N; i++) {
-            if(map[i][idx] == 1) {
-                map[i][idx] = 0;
-            } else if(map[i][idx] > 1) {
-
+    static void copyMap(int origin[][], int copy_map[][]) {
+        for(int i=0; i<W; i++) {
+            for(int j=0; j<H; j++) {
+                copy_map[j][i] = origin[j][i];
             }
         }
     }
 
-    static void breakBrick(int i, int idx, int map[][]) {
-        int dx[] = {1,-1,0,0};
-        int dy[] = {0,0,1,-1};
-        int x = i;
-        int y = idx;
+    static void setMarble(int cnt, int[][] map) {
+        if(cnt==N) {
+            minBrick = Math.min(minBrick, findBrick(map));
+            return;
+        }
+        
+        int copy_map[][] = new int[H][W];
+        for(int i=0; i<W; i++) {
+            for(int j=0; j<H; j++) {
+                if(map[j][i]==0) continue;
+                
+                copyMap(map, copy_map);
+                breakBrick(j, i, copy_map);
+                downTobrick(copy_map);
+                setMarble(cnt+1, copy_map);
+                break;
+            }
+        }
+
+        // minBrick = Math.min(minBrick, findBrick(map));
+    }
+
+    static void breakBrick(int j, int i, int copy_map[][]) {
+        int dx[] = {1, -1, 0, 0};
+        int dy[] = {0, 0, 1, -1};
+        int x = j;
+        int y = i;
+        int num = copy_map[x][y];
         
         Queue<Node> que = new LinkedList<>();
         que.add(new Node(x,y));
+
         while(!que.isEmpty()) {
             Node n = que.poll();
+            num = copy_map[n.x][n.y];
+            copy_map[n.x][n.y] = 0;
             
             for(int dir=0; dir<4; dir++) {
-                for(int cnt=1; cnt<map[n.x][n.y]; cnt++) {
-                    
-                }                
+                for(int cnt=1; cnt<num; cnt++) {
+                    x = n.x + dx[dir] * cnt;
+                    y = n.y + dy[dir] * cnt;
+                    if(wall(x,y)) break;
+                    if(copy_map[x][y]==0) continue;
+                    else {
+                        que.add(new Node(x, y));
+                    }
+                }
             }
         }
+    }
+
+    static int findBrick(int[][] copy_map) {
+        int cntBrick = 0;
+        for(int i=0; i<H; i++) {
+            for(int j=0; j<W; j++) {
+                if(copy_map[i][j]!=0) cntBrick += 1;
+            }
+        }
+
+        return cntBrick;
     }
 
     static boolean wall(int x, int y) {
         if(x<0 || x>=H || y<0 || y>=W) return true;
         return false;
+    }
+
+    static void printMap(int[][] copy_map) {
+        for(int i=0; i<H; i++) {
+            for(int j=0; j<W; j++) {
+                System.out.print(copy_map[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------");
     }
 
     static public void main(String args[]) throws IOException {
@@ -90,8 +129,21 @@ public class S5656 {
             N = Integer.parseInt(input[0]);
             W = Integer.parseInt(input[1]);
             H = Integer.parseInt(input[2]);
+            
+            int[][] map = new int[H][W];
+            minBrick = Integer.MAX_VALUE;
 
-            int map[][] = new int[H][W];
+            for(int i=0; i<H; i++) {
+                input = br.readLine().split(" ");
+                for(int j=0; j<W; j++) {
+                    map[i][j] = Integer.parseInt(input[j]);
+                }
+            }
+
+            setMarble(0, map);
+            
+            if(minBrick == Integer.MAX_VALUE) minBrick = 0;
+            System.out.println("#"+tc+" "+minBrick);
         }
     }
 }
