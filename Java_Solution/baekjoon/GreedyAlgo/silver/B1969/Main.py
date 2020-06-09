e7e5aca2002b52534a88fe2f89a18f7186d7e079
Heon_9u <frtt0608@naver.com>
2020-06-09 22:41:54 +0900

    200609|백준|DNA|문제에 특화된 풀이

---
+++ Java_Solution/baekjoon/GreedyAlgo/silver/B1969/Main.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open("input.txt")
+
+def main():
+    N, M = map(int, input().split(" "))
+    data = [[0, 0, 0, 0] for _ in range(0, M)]
+    dictData = {"A":0, "C":1, "G":2, "T":3}
+    alpha = ["A", "C", "G", "T"]
+
+    minCnt = 0
+    minData = ""
+
+    for _ in range(0, N):
+        dna = input()
+        for i,j in enumerate(dna):
+            data[i][dictData[j]] += 1
+    
+    for dist in data:
+        print(dist)
+        maxDist = max(dist)
+        minData += alpha[dist.index(maxDist)]
+        minCnt += N - maxDist
+
+    print(minData)
+    print(minCnt)
+
+main()
