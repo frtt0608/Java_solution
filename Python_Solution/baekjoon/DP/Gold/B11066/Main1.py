21e2728ec221e337896a200ae5e7680e2fa32a12
Heon_9u <frtt0608@naver.com>
2020-06-21 18:20:28 +0900

    200621|백준|파일 합치기|Knuth's optimization |어떠한 구간 쪼개는 형태의 동적계획법을 최적화하는 알고리즘으로 사각 부등식과 점화식이 만족하는 경우 사용할 수 있다|알고리즘의 흐름도는 이해했지만 실제로 이런 문제가 나온다면 활용할 수 있을 지 미지수

--- Python_Solution/baekjoon/DP/Gold/B11066/Main1.py
+++ Python_Solution/baekjoon/DP/Gold/B11066/Main1.py
@@ -1,5 +1,5 @@
 # 파일 합치기
-# Knuth's Optimization - O(N^3)
+# Knuth's Optimization - O(N^2)
 
 import sys
 import pprint
@@ -12,27 +12,41 @@ for _ in range(T):
     K = int(input())
     fileSize = list(map(int, input().split()))
     # fileSize.sort(reverse = True)
-    dpMap = [[0] * K for _ in range(K)]
+    dpMap = [[0] * (K+1) for _ in range(K+1)]
     priceSum = [0] * K
+    cost = [[0] * K for _ in range(K)]
+
     for i in range(K):
+        cost[i][i] = i # 단조성과 사각 부등식의 배열 cost 초기화
+
         if i == 0:
             priceSum[i] = fileSize[i]
         else:
             priceSum[i] = priceSum[i-1] + fileSize[i]
 
+
     # i는 구간의 길이
     for i in range(1, K):
         # j는 구간이 시작하는 index
         for j in range(0, K):
+
             if i+j == K: break
-            dpMap[j][j+i] = sys.maxsize
+            dpMap[j][i+j] = sys.maxsize
 
             # k는 구간을 둘로 나누는 역할
-            for k in range(j, j+i):
+            for k in range(cost[j][i+j-1], cost[j+1][i+j]+1):
+
+                temp = 0
+
                 if j == 0:
-                    dpMap[j][j+i] = min(dpMap[j][j+i], dpMap[j][k] + dpMap[k+1][j+i] + priceSum[i+j])
+                    temp = dpMap[j][k] + dpMap[k+1][j+i] + priceSum[i+j]
                 else:
-                    dpMap[j][j+i] = min(dpMap[j][j+i], dpMap[j][k] + dpMap[k+1][i+j] + priceSum[i+j] - priceSum[j-1])
-                
+                    temp = dpMap[j][k] + dpMap[k+1][i+j] + priceSum[i+j] - priceSum[j-1]
+
+                if dpMap[j][i+j] > temp:
+                    dpMap[j][i+j] = temp
+                    cost[j][i+j] = k
     
+    # pprint.pprint(cost)
+    # pprint.pprint(dpMap)
     print(dpMap[0][K-1])
