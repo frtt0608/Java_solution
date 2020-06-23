fff13e70f101ea93c3f9b3a47e0ac899affbdb9e
Heon_9u <frtt0608@naver.com>
2020-06-23 17:01:24 +0900

    200623|백준|동전1|동전교환|점화식 세우기 공부하자

--- Python_Solution/baekjoon/DP/B2293/Main.py
+++ Python_Solution/baekjoon/DP/B2293/Main.py
@@ -7,5 +7,14 @@ N, K = map(int, input().split())
 
 coins = [int(input()) for _ in range(N)]
 coins.sort()
-dpMap = [[0] * K for _ in range(N)]
+dpArr = [0] * (K+1)
+dpArr[0] = 1
 
+for coin in coins:
+    coin =  int(coin)
+
+    for i in range(coin, K+1):  
+        dpArr[i] += dpArr[i - coin]
+
+print(dpArr)
+print(dpArr[K])
