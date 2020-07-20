70aae909b2fe1750ed4997a7bdd272024e11d548
Heon_9u <frtt0608@naver.com>
2020-07-20 23:45:21 +0900

    200720|백준|문자열처리|입력값을 아스키코드로 변환

---
+++ Java_Solution/baekjoon/String/Bronze/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    static public void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.read());
+    }
+}
