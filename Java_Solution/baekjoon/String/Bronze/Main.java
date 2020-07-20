cbae05d2c39bbae7f30cbc33d1c7cf6a8c121b08
Heon_9u <frtt0608@naver.com>
2020-07-21 00:03:32 +0900

    200721|백준|문자열처리|문자열 공백처리

--- Java_Solution/baekjoon/String/Bronze/Main.java
+++ Java_Solution/baekjoon/String/Bronze/Main.java
@@ -1,10 +1,18 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     static public void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println(br.read());
+        String input = br.readLine().trim();
+
+        if(input.equals("")) {
+            System.out.println(0);
+        } else {
+            String[] arr = input.split(" ");
+            System.out.println(arr.length);
+        }
     }
 }
