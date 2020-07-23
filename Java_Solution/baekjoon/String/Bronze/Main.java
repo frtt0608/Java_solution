76a5f52b72467980f0c919b446dd531b89ec6a13
Heon_9u <frtt0608@naver.com>
2020-07-23 22:03:54 +0900

    200723|백준|문자열처리|단어공부

--- Java_Solution/baekjoon/String/Bronze/Main.java
+++ Java_Solution/baekjoon/String/Bronze/Main.java
@@ -8,19 +8,36 @@ public class Main {
 
         String input = br.readLine();
         int[] alpha = new int[26];
-        Arrays.fill(alpha, -1);
-
+        int idx = 0;
+        int maxCnt = 0;
+        int maxIdx = -1;
+        char res = '?';
+        
         for(int i=0; i<input.length(); i++) {
             char chr = input.charAt(i);
-            int idx = chr - 'a';
 
-            if(alpha[idx] == -1) {
-                alpha[idx] = i;
+            if((int)chr >= 97) {
+                idx = chr - 'a';
+            } else {
+                idx = chr - 'A';
             }
+
+            alpha[idx] += 1;
+            maxCnt = Math.max(maxCnt, alpha[idx]);
         }
 
         for(int i=0; i<26; i++) {
-            System.out.print(alpha[i] + " ");
+            if(maxCnt == alpha[i]) {
+                if(maxIdx == -1) {
+                    maxIdx = i;
+                    res = (char)(i + 65);
+                } else {
+                    res = '?';
+                    break;
+                }
+            }
         }
+
+        System.out.println(res);
     }
 }
