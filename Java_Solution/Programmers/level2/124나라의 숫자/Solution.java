d1c194cae4985cc2cb8b2536ec63abd06ad89492
Heon_9u <frtt0608@naver.com>
2020-08-08 22:13:25 +0900

    200808|프로그래머스|level2|124나라의 숫자|아직

---
+++ Java_Solution/Programmers/level2/124나라의 숫자/Solution.java
@@ -0,0 +1,14 @@
+// 124
+
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        
+    }
+}
+
