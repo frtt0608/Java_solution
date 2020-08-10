08b37e48accf6e212c10037f879c7e608c9e70db
Heon_9u <frtt0608@naver.com>
2020-08-10 21:47:43 +0900

    200810|프로그래머스|124나라의 숫자|구현

--- Java_Solution/Programmers/level2/124나라의 숫자/Solution.java
+++ Java_Solution/Programmers/level2/124나라의 숫자/Solution.java
@@ -1,14 +1,19 @@
-// 124
-
-import java.util.*;
-import java.io.*;
-
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
         
+        while(n>0) {
+            int remain = n%3;
+            n /= 3;
+            
+            if(remain == 0) {
+                remain = 4;
+                n -= 1;
+            }
+            
+            sb.append(remain);
+        }
+        
+        return sb.reverse().toString();
     }
 }
-
