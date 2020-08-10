cfa38c29fa3072805e7e7433b5e016af36c25bc7
Heon_9u <frtt0608@naver.com>
2020-08-10 23:44:38 +0900

    200810|프로그래머스|줄서는방법|규칙을 찾아서|처음에 k에서 왜 1을 뺴는지 아는게 중요하다

---
+++ Java_Solution/Programmers/level3/줄서는방법/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        LinkedList<Integer> numList = new LinkedList<>();
+        long fact = 1;
+        for(int i=1; i<=n; i++) {
+            fact *= i;
+            numList.add(i);
+        }
+        
+        k -= 1; // k는 1부터 시작하므로!!
+        int i = 0;
+        while(i<n) {
+            fact /= (n-i);
+            long idx = k/fact;
+            answer[i] = numList.get((int)idx);
+            numList.remove((int)idx);
+            i++;
+            
+            k %= fact;
+        }
+        
+        return answer;
+    }
+}
