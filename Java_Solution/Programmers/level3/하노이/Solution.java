a888b13053530ee899c46377858c61115604dfba
Heon_9u <frtt0608@naver.com>
2020-08-11 12:54:07 +0900

    200811|프로그래머스|하노이의 탑|재귀 활용

---
+++ Java_Solution/Programmers/level3/하노이/Solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    ArrayList<int[]> routeList;
+    
+    public void hanoi(int n, int start, int end, int mid) {
+        int[] move = {start, end};
+        
+        if(n == 1) {
+            routeList.add(move);
+            return;
+        } else {
+            hanoi(n-1, start, mid, end);
+            routeList.add(move);
+            hanoi(n-1, mid, end, start);
+        }
+    }
+    
+    public int[][] solution(int n) {
+        routeList = new ArrayList<>();
+        
+        hanoi(n, 1, 3, 2);
+        
+        int[][] answer = new int[routeList.size()][2];
+        
+        for(int i=0; i<routeList.size(); i++) {
+            int[] route = routeList.get(i);
+            answer[i][0] = route[0];
+            answer[i][1] = route[1];
+        }
+        
+        return answer;
+    }
+}
