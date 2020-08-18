cd2fa4a22a3132ba4c0b58391c75a90347dd17ea
Heon_9u <frtt0608@naver.com>
2020-08-18 20:55:23 +0900

    200818|프로그래머스|level1|나누어떨어지는 숫자배열|Arrays메소드 활용

--- Java_Solution/Programmers/level1/나누어떨어지는숫자배열.java
+++ Java_Solution/Programmers/level1/나누어떨어지는숫자배열.java
@@ -1,28 +1,43 @@
 import java.util.*;
 
+// class Solution {
+//     public int[] solution(int[] arr, int divisor) {
+//         LinkedList<Integer> numList = new LinkedList<>();
+        
+//         for(int num: arr) {
+//             if(num % divisor == 0) {
+//                 numList.add(num);
+//             }
+//         }
+        
+//         if(numList.size() == 0) {
+//             int[] answer = {-1};
+//             return answer;
+//         } else {
+//             int[] answer = new int[numList.size()];
+//             int idx = 0;
+//             while(!numList.isEmpty()) {
+//                 answer[idx] = numList.poll();
+//                 idx++;
+//             }
+//             Arrays.sort(answer);
+//             System.out.println(Arrays.toString(answer));
+//             return answer;
+//         }
+//     }
+// }
+
+
+// Arrays의 메소드 활용
 class Solution {
     public int[] solution(int[] arr, int divisor) {
-        LinkedList<Integer> numList = new LinkedList<>();
+        int[] answer = Arrays.stream(arr).filter(num -> num % divisor == 0).toArray();
         
-        for(int num: arr) {
-            if(num % divisor == 0) {
-                numList.add(num);
-            }
-        }
-        
-        if(numList.size() == 0) {
-            int[] answer = {-1};
-            return answer;
-        } else {
-            int[] answer = new int[numList.size()];
-            int idx = 0;
-            while(!numList.isEmpty()) {
-                answer[idx] = numList.poll();
-                idx++;
-            }
+        if(answer.length == 0) answer = new int[] {-1};
+        else {
             Arrays.sort(answer);
-            System.out.println(Arrays.toString(answer));
-            return answer;
         }
+        
+        return answer;
     }
 }
