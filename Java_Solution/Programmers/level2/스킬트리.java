55673929797136670f44eb01ed4d924d9f58a391
Heon_9u <frtt0608@naver.com>
2020-08-19 23:45:01 +0900

    200819|프로그래머스|level2|스킬트리

---
+++ Java_Solution/Programmers/level2/스킬트리.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        char[] skills = skill.toCharArray();
+        ArrayList<Character> skillList = new ArrayList<>();
+        for(int i=0; i<skills.length; i++) {
+            skillList.add(skills[i]);
+        }
+        
+        loop:
+        for(int i=0; i<skill_trees.length; i++) {
+            int idx = 0;
+            String tree = skill_trees[i];
+            
+            for(int j=0; j<tree.length(); j++) {
+                if(skillList.contains(tree.charAt(j))) {
+                    // System.out.println(tree.charAt(j) + ", " + skillList.get(idx) );
+                    if(skillList.get(idx) == tree.charAt(j)) {
+                        
+                        idx++;
+                    } else continue loop;
+                }
+            }
+            
+            answer++;
+        }
+        
+        return answer;
+    }
+}
