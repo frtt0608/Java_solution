12b6332af664f183ea0376bb927cc1b859983afb
Heon_9u <frtt0608@naver.com>
2020-09-15 12:53:18 +0900

    200915|백준|최솟값찾기|슬라이딩 윈도우

---
+++ Java_Solution/baekjoon/Platinum/최솟값찾기/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static public void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int[] numArr = new int[N];
+        
+        Deque<Integer> deq = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<N; i++) {
+            numArr[i] = Integer.parseInt(st.nextToken());
+            while(!deq.isEmpty() && numArr[deq.getLast()] > numArr[i]) {
+                deq.removeLast();
+            }
+            deq.offer(i);
+            if(deq.getFirst() < i-L+1) deq.removeFirst();
+            sb.append(numArr[deq.getFirst()]).append(" ");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
