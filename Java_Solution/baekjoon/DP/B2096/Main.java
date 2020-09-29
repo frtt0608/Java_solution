81f8de5093660251837324224bb07b893b75a194
Heon_9u <frtt0608@naver.com>
2020-09-29 23:08:02 +0900

    200929|백준|dp|슬라이딩윈도우|내려가기|걸린시간과 메모리  개선

--- Java_Solution/baekjoon/DP/B2096/Main.java
+++ Java_Solution/baekjoon/DP/B2096/Main.java
@@ -3,57 +3,58 @@ import java.io.*;
 
 public class Main {
     static int N;
-    static int[][] map, dpMap_max, dpMap_min;
 
     static public void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-        map = new int[N][3];
-        dpMap_max = new int[N][3];
-        dpMap_min = new int[N][3];
-        
-        for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<3; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        dpMap_max[0][0] = map[0][0];
-        dpMap_max[0][1] = map[0][1];
-        dpMap_max[0][2] = map[0][2];
-
-        dpMap_min[0][0] = map[0][0];
-        dpMap_min[0][1] = map[0][1];
-        dpMap_min[0][2] = map[0][2];
-
-        for(int i=1; i<N; i++) {
-            dpMap_max[i][0] = Math.max(dpMap_max[i-1][1] + map[i][0], dpMap_max[i-1][0] + map[i][0]);
-            dpMap_max[i][1] = Math.max(dpMap_max[i-1][0] + map[i][1], dpMap_max[i-1][1] + map[i][1]);
-            dpMap_max[i][1] = Math.max(dpMap_max[i][1], dpMap_max[i-1][2] + map[i][1]);
-            dpMap_max[i][2] = Math.max(dpMap_max[i-1][1] + map[i][2], dpMap_max[i-1][2] + map[i][2]);
-
-            dpMap_min[i][0] = Math.min(dpMap_min[i-1][1] + map[i][0], dpMap_min[i-1][0] + map[i][0]);
-            dpMap_min[i][1] = Math.min(dpMap_min[i-1][0] + map[i][1], dpMap_min[i-1][1] + map[i][1]);
-            dpMap_min[i][1] = Math.min(dpMap_min[i][1], dpMap_min[i-1][2] + map[i][1]);
-            dpMap_min[i][2] = Math.min(dpMap_min[i-1][1] + map[i][2], dpMap_min[i-1][2] + map[i][2]);
-        }
-
+        int[] map = new int[3];
+        int[] dpMax = new int[3];
+        int[] dpMin = new int[3];
+        int[] dpTemp = new int[3];
         int max = 0;
         int min = Integer.MAX_VALUE;
+        
+        for(int i=0; i<N; i++) {
+            String str = br.readLine();
+            for(int j=0; j<3; j++) {
+                map[j] = str.charAt(j+j) - '0';
+                if(i == 0) {
+                    dpMax[j] = map[j];
+                    dpMin[j] = map[j];
+                }
+            }
 
-        // for(int i=0; i<N; i++) {
-        //     System.out.println(Arrays.toString(dpMap_min[i]));
-        // }
+            if(i == 0) continue;
 
-        for(int i=0; i<3; i++) {
-            max = Math.max(max, dpMap_max[N-1][i]);
-            min = Math.min(min, dpMap_min[N-1][i]);
+            dpTemp[0] = map[0] + Math.max(dpMax[0], dpMax[1]);
+            dpTemp[1] = map[1] + Math.max(dpMax[0], Math.max(dpMax[1], dpMax[2]));
+            dpTemp[2] = map[2] + Math.max(dpMax[1], dpMax[2]);
+
+            dpMax[0] = dpTemp[0];
+            dpMax[1] = dpTemp[1];
+            dpMax[2] = dpTemp[2];
+
+            dpTemp[0] = map[0] + Math.min(dpMin[0], dpMin[1]);
+            dpTemp[1] = map[1] + Math.min(dpMin[0], Math.min(dpMin[1], dpMin[2]));
+            dpTemp[2] = map[2] + Math.min(dpMin[1], dpMin[2]);
+
+            dpMin[0] = dpTemp[0];
+            dpMin[1] = dpTemp[1];
+            dpMin[2] = dpTemp[2];
         }
 
-        System.out.println(max + " " + min);
+        for(int i=0; i<3; i++) {
+            max = Math.max(max, dpMax[i]);
+            min = Math.min(min, dpMin[i]);
+        }
+
+        bw.write(max + " " + min);
+        bw.flush();
+        bw.close();
     }
 }
