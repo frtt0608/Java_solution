1077d0ec9f72d4c484f1a6b3ed1a6e755e5ff187
Heon_9u <frtt0608@naver.com>
2020-10-03 12:22:50 +0900

    201003|프로그래머스level3|길찾기게임review

--- Java_Solution/Programmers/level3/길찾기게임.java
+++ Java_Solution/Programmers/level3/길찾기게임.java
@@ -3,28 +3,32 @@ import java.util.*;
 public class 길찾기게임 {
 
     class Solution {
-        int len, idx;
-        Node[] tree;
+        int nodeLen;
+        int idx;
         int[][] answer;
+        Node[] tree;
         
-        public void makeTree(Node root, Node insert) {
-            if(insert.x < root.x) {
-                if(root.left == null) root.left = insert;
-                else
-                    makeTree(root.left, insert);
-                
+        public void makeTree(Node root, Node insertNode) {
+            if(insertNode.x < root.x) {
+                if(root.left == null) {
+                    root.left = insertNode;
+                } else {
+                    makeTree(root.left, insertNode);
+                }
             } else {
-                if(root.right == null) root.right = insert;
-                else
-                    makeTree(root.right, insert);
+                if(root.right == null) {
+                    root.right = insertNode;
+                } else {
+                    makeTree(root.right, insertNode);
+                }
             }
         }
         
         public void preorder(Node root) {
             if(root != null) {
-                answer[0][idx++] = root.root;
+                answer[0][idx++] = root.value;
                 preorder(root.left);
-                preorder(root.right);   
+                preorder(root.right);
             }
         }
         
@@ -32,19 +36,17 @@ public class 길찾기게임 {
             if(root != null) {
                 postorder(root.left);
                 postorder(root.right);
-                answer[1][idx++] = root.root;
+                answer[1][idx++] = root.value;
             }
         }
         
         public int[][] solution(int[][] nodeinfo) {
-            len = nodeinfo.length;
-            answer = new int[2][len];
-            tree = new Node[len];
+            nodeLen = nodeinfo.length;
+            answer = new int[2][nodeLen];
+            tree = new Node[nodeLen];
             
-            for(int i=0; i<len; i++) {
-                int x = nodeinfo[i][0];
-                int y = nodeinfo[i][1];
-                tree[i] = new Node(x, y, i+1);
+            for(int i=0; i<nodeLen; i++) {
+                tree[i] = new Node(nodeinfo[i][0], nodeinfo[i][1], i+1);
             }
             
             Arrays.sort(tree, new Comparator<Node>() {
@@ -54,8 +56,8 @@ public class 길찾기게임 {
                 }
             });
             
-            for(int i=1; i<len; i++) {
-                makeTree(tree[0], tree[i]);
+            for(int i=1; i<nodeLen; i++) {
+                makeTree(tree[0], tree[i]);   
             }
             
             idx = 0;
@@ -69,14 +71,15 @@ public class 길찾기게임 {
         public class Node {
             int x;
             int y;
-            int root;
+            int value;
+            
             Node left = null;
             Node right = null;
             
-            Node(int x, int y, int root) {
+            Node(int x, int y, int value) {
                 this.x = x;
                 this.y = y;
-                this.root = root;
+                this.value = value;
             }
         }
     }
