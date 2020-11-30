5e73946d64dd39f9aefcf0754543a80cfa0f8406
Heon_9u <frtt0608@naver.com>
2020-11-30 12:24:26 +0900

    201130|swea|S2115|벌꿀채집|구현문제|시간과 메모리에 너무 집착하지말고 풀기

--- Java_Solution/swea/A+ 대비/S2115/Solution.java
+++ Java_Solution/swea/A+ 대비/S2115/Solution.java
@@ -4,60 +4,99 @@ import java.io.*;
 /**
  * Solution
  */
+
+ // DFS, bruteforce
+
 public class Solution {
-    static int N, M, C, maxRes;
-    static int[][] map;
+    static int T, N, M, C, maxIncome;
+    static int honey1, honey2, money1, money2;
+    static int[][] honey;
 
-    static void choiceHoeny(int cnt, int idx, int res, boolean[][] visited) {
-        if(cnt==2) {
-            maxRes = Math.max(maxRes, res);
-            return;
-        }
-
-        for(int i=idx; i<N; i++) {
-            int honeyCnt = 0;
-            LinkedList<Integer> honeyList = new LinkedList<>();
-
-            for(int j=0; j<N; j++) {
-                if(visited[i][j]) continue;
-                visited[i][j] = true;
-                honeyCnt++;
-                honeyList.add(map[i][j]);
-                if(honeyCnt==M) {
-
-                }
+    public static void getMaxMoney(int si, int sj) {
+        int j = 0;
+        for(int i=si; i<N; i++) {
+            if(i == si) j=sj;
+            else j=0;
+            for(; j<N; j++) {
+                if(j+M-1 < N) {
+                    honey2 = 0;
+                    getMaxHoney2(Arrays.copyOfRange(honey[i], j, j+M), 0, 0, new int[M], 0);
+                    // System.out.println(honey1 + ", " + honey2);
+                    maxIncome = Math.max(maxIncome, honey1 + honey2);
+                } else
+                    break;
             }
         }
     }
 
-    static void checkHoney(int i, int j) {
-        
+    public static void getMaxHoney2(int[] honeyArr, int sum, int idx, int[] arr, int ai) {
+        if(sum <= C) {
+            money2 = 0;
+            for(int a: arr) 
+                money2 += Math.pow(a, 2);
+
+            honey2 = Math.max(honey2, money2);
+        }
+
+        for(int i=idx; i<M; i++) {
+            if(sum + honeyArr[i] <= C) {
+                arr[ai] = honeyArr[i];
+                getMaxHoney2(honeyArr, sum+honeyArr[i], i+1, arr, ai+1);
+                arr[ai] = 0;
+            }
+        }
     }
 
-    static public void main(String[] args) throws IOException {
+    public static void getMaxHoney1(int[] honeyArr, int sum, int idx, int[] arr, int ai) {
+        if(sum <= C) {
+            money1 = 0;
+            for(int a: arr) 
+                money1 += Math.pow(a, 2);
+
+            honey1 = Math.max(honey1, money1);
+        }
+
+        for(int i=idx; i<M; i++) {
+            if(sum + honeyArr[i] <= C) {
+                arr[ai] = honeyArr[i];
+                getMaxHoney1(honeyArr, sum+honeyArr[i], i+1, arr, ai+1);
+                arr[ai] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
 
+        T = Integer.parseInt(br.readLine());
         for(int tc=1; tc<=T; tc++) {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             C = Integer.parseInt(st.nextToken());
+            maxIncome = 0;
 
-            map = new int[N][N];
-
+            honey = new int[N][N];
             for(int i=0; i<N; i++) {
                 st = new StringTokenizer(br.readLine());
                 for(int j=0; j<N; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
+                    honey[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            maxRes = 0;
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<N; j++) {
+                    if(j+M-1 < N) {
+                        honey1 = 0;
+                        getMaxHoney1(Arrays.copyOfRange(honey[i], j, j+M), 0, 0, new int[M], 0);
+                        getMaxMoney(i, j+M);
+                    }
+                }
+            }
 
-
+            System.out.println("#" + tc + " " + maxIncome);
         }
     }
 }
