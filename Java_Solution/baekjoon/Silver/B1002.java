74c4743f7808d1dca905d5839ae51f0fd63c2183
Heon_9u <frtt0608@naver.com>
2021-02-05 12:43:50 +0900

    210205 | 백준|터렛|수학단계 문제 완료

--- Java_Solution/baekjoon/Silver/B1002.java
+++ Java_Solution/baekjoon/Silver/B1002.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class B1002 {
 
     static public void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
