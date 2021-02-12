a905a9610419cb59d40f531725b437d4c9eec8a4
Heon_9u <frtt0608@naver.com>
2021-02-12 14:09:51 +0900

    210212 | 백준|BFS|이분그래프reivew|정점 색칠하기

---
+++ Java_Solution/baekjoon/Review_Solution/B1707/Main.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int V, E;
+    static int[] visited;
+    static ArrayList<Integer>[] graph;
+    static String result;
+
+    public static void checkBipartiteGraph(int v) {
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(v);
+        visited[v] = 1;
+
+        while(!que.isEmpty()) {
+            int st = que.poll();
+
+            for(int et : graph[st]) {
+                if(visited[et] == 0) {
+                    visited[et] =  (-1) * visited[st];
+                    que.offer(et);
+                } else if(visited[st] == visited[et]) {
+                    result = "NO";
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for(int tc=1; tc<=T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(st.nextToken());
+            E = Integer.parseInt(st.nextToken());
+            result = "YES";
+            visited = new int[V+1];
+
+            graph = new ArrayList[V+1];
+            for(int i=1; i<V+1; i++) {
+                graph[i] = new ArrayList<>();
+            }
+
+            for(int i=0; i<E; i++) {
+                st = new StringTokenizer(br.readLine());
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                
+                graph[s].add(e);
+                graph[e].add(s);
+            }
+
+            for(int i=1; i<V+1; i++) {
+                if(result.equals("NO")) break;
+                if(visited[i] == 0) checkBipartiteGraph(i);
+            }
+            
+            System.out.println(result);
+        }
+    }
+}
