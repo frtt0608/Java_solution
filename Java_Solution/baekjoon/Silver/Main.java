67b721e245ec7c44f4d228cb8d9efd6eca5f16d2
Heon_9u <frtt0608@naver.com>
2021-02-12 15:37:46 +0900

    210212 | 백준|DP|memoization 배열 미리 setting|출력이 많은 경우 StringBuilder 활용해서 수행시간 단축시키기

--- Java_Solution/baekjoon/Silver/Main.java
+++ Java_Solution/baekjoon/Silver/Main.java
@@ -2,29 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static final int SIZE = 20;
     static int a, b, c;
     static int[][][] map;
     
     public static int w(int a, int b, int c) {
-        if(a<=0 || b<=0 || c<=0) return 1;
-        if(a>20 || b>20 || c>20) return w(20, 20, 20);
-        if(map[a][b][c] != 0) return map[a][b][c];
+        if(a<=0 || b<=0 || c<=0) 
+            return 1;
+        if(a>20 || b>20 || c>20) 
+            return w(20, 20, 20);
+        if(a<b && b<c) 
+            return map[a][b][c-1] + map[a][b-1][c-1] - map[a][b-1][c];
+        
+        return map[a-1][b][c] + map[a-1][b-1][c] + map[a-1][b][c-1] - map[a-1][b-1][c-1];
+    }
 
-        if(a<b && b<c) {
-            map[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
-        } else {
-            map[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+    public static void setMap() {
+        map = new int[SIZE+1][SIZE+1][SIZE+1];
+
+        for(int a=0; a<=SIZE; a++) {
+            for(int b=0; b<=SIZE; b++) {
+                for(int c=0; c<=SIZE; c++) {
+                    map[a][b][c] = w(a, b, c);
+                }
+            }
         }
-
-        return map[a][b][c];
     }
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-        map = new int[21][21][21];
-    
+        setMap();
+
         while(true) {
             st = new StringTokenizer(br.readLine());
             a = Integer.parseInt(st.nextToken());
@@ -34,8 +45,10 @@ public class Main {
             if(a==-1 && b==-1 && c==-1) break;
 
             int result = w(a, b, c);
-
-            System.out.println(String.format("w(%d, %d, %d) = %d", a, b, c, result));
+            sb.append("w(").append(a).append(", ").append(b).append(", ").append(c)
+                .append(") = ").append(result).append("\n");
         }
+
+        System.out.println(sb.toString());
     }
 }
