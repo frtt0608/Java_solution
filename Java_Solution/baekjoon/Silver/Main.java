965c55aec53260fa5c9f76dbd90c39477ce2c229
Heon_9u <frtt0608@naver.com>
2021-02-12 15:28:17 +0900

    210212 | 백준|DP|재귀함수에 memoization 활용|조건문 순서, 조건에 따른 배열 크기 확인하기

--- Java_Solution/baekjoon/Silver/Main.java
+++ Java_Solution/baekjoon/Silver/Main.java
@@ -2,86 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, sx, sy, tx, ty, minCount;
-    static int[][] chess, countMap;
+    static int a, b, c;
+    static int[][][] map;
+    
+    public static int w(int a, int b, int c) {
+        if(a<=0 || b<=0 || c<=0) return 1;
+        if(a>20 || b>20 || c>20) return w(20, 20, 20);
+        if(map[a][b][c] != 0) return map[a][b][c];
 
-    static class Node {
-        int x, y;
-        int count;
-
-        Node(int x, int y, int count) {
-            this.x = x;
-            this.y = y;
-            this.count = count;
+        if(a<b && b<c) {
+            map[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+        } else {
+            map[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
         }
-    }
 
-    public static boolean isWall(int x, int y) {
-        if(x<0 || x>=N || y<0 || y>=N) return true;
-        return false;
-    }
-
-    public static void moveKnight() {
-        int[] dx = {-2,-1,1,2,2,1,-1,-2};
-        int[] dy = {1,2,2,1,-1,-2,-2,-1};
-        Queue<Node> que = new LinkedList<>();
-        que.add(new Node(sx, sy, 0));
-        countMap[sx][sy] = 0;
-
-        while(!que.isEmpty()) {
-            Node nd = que.poll();
-
-            if(nd.x == tx && nd.y == ty) {
-                minCount = countMap[tx][ty];
-                return;
-            }
-
-            for(int dir=0; dir<8; dir++) {
-                int nx = nd.x + dx[dir];
-                int ny = nd.y + dy[dir];
-
-                if(isWall(nx, ny)) continue;
-
-                if(countMap[nx][ny] > countMap[nd.x][nd.y] + 1) {
-                    countMap[nx][ny] = countMap[nd.x][nd.y] + 1;
-                    que.add(new Node(nx, ny, countMap[nx][ny]));
-                }
-            }
-        }
-    }
-
-    public static void setCountMap() {
-        for(int i=0; i<N; i++) {
-            Arrays.fill(countMap[i], Integer.MAX_VALUE);
-        }
+        return map[a][b][c];
     }
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        map = new int[21][21][21];
     
-        int T = Integer.parseInt(br.readLine());
-        for(int tc=1; tc<=T; tc++) {
-            N = Integer.parseInt(br.readLine());
-            chess = new int[N][N];
-            countMap = new int[N][N];
-            minCount = 0;
-
+        while(true) {
             st = new StringTokenizer(br.readLine());
-            sx = Integer.parseInt(st.nextToken());
-            sy = Integer.parseInt(st.nextToken());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
 
-            st = new StringTokenizer(br.readLine());
-            tx = Integer.parseInt(st.nextToken());
-            ty = Integer.parseInt(st.nextToken());
+            if(a==-1 && b==-1 && c==-1) break;
 
-            if(sx != tx || sy != ty) {
-                setCountMap();
-                moveKnight();
-            }
+            int result = w(a, b, c);
 
-            System.out.println(minCount);
+            System.out.println(String.format("w(%d, %d, %d) = %d", a, b, c, result));
         }
     }
 }
