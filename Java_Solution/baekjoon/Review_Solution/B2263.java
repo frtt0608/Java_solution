456ae6c3dc076de18948216925a5b0e78ed27b20
Heon_9u <frtt0608@naver.com>
2021-02-18 12:14:44 +0900

    210218 | 백준review|트리순회|재귀 index 고려하기

--- Java_Solution/baekjoon/Review_Solution/B2263.java
+++ Java_Solution/baekjoon/Review_Solution/B2263.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class B2263 {
     static int N;
     static int[] inOrder, postOrder, inOrderIndex;
     static StringBuilder preOrder;
