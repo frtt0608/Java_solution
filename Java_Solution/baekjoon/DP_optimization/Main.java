2fdd21b2a53f2e124929ca72f6b7b655e7cbabf3
Heon_9u <frtt0608@naver.com>
2021-03-02 11:01:25 +0900

    210302 | 백준review|dp|메모이제이션|백트래킹

---
+++ Java_Solution/baekjoon/DP_optimization/Main.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static int[] weight;
+    static boolean[][] isVerifiable;
+
+    public static void findPossibleWeight(int idx, int sumWeight) {
+        if(isVerifiable[idx][sumWeight]) return;
+        isVerifiable[idx][sumWeight] = true;
+        if(idx >= N) return;
+
+        findPossibleWeight(idx+1, sumWeight + weight[idx]);
+        findPossibleWeight(idx+1, sumWeight);
+        findPossibleWeight(idx+1, Math.abs(sumWeight - weight[idx]));
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        weight = new int[N];
+        int size = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            weight[i] = Integer.parseInt(st.nextToken());
+            size += weight[i];
+        }
+        isVerifiable = new boolean[N+1][size+1];
+
+        findPossibleWeight(0, 0);
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<M; i++) {
+            int checkWeight = Integer.parseInt(st.nextToken());
+    
+            if(checkWeight <= size && isVerifiable[N][checkWeight]) {
+                sb.append("Y ");
+            } else sb.append("N ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
