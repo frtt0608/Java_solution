8574c859696dff85d251210d5026106e5584a662
Heon_9u <frtt0608@naver.com>
2021-04-05 22:26:43 +0900

    210405 |백준review|원형 배열에서의 dp

--- Java_Solution/baekjoon/Review_Solution/B2482.java
+++ Java_Solution/baekjoon/Review_Solution/B2482.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class B2482 {
     static final int MOD = 1000000003;
     static int N, K;
     static int[][] dp;
@@ -11,14 +11,16 @@ public class Main {
         if(n<0 || k<0) 
             return 0;
 
-        if(dp[n][k] != 0) {
+        if(dp[n][k] != -1) {
             return dp[n][k];
         } else if(k == 1) {
-            return n;
+            return dp[n][k] = n;
+        } else if(k == 0) {
+            return dp[n][k] = 1;
         }
 
         if(2*k > n+1) {
-            return 0;
+            return dp[n][k] = 0;
         } else {
             dp[n][k] = (findTotalCase(n-2, k-1) + findTotalCase(n-1, k))%MOD;
         }
@@ -30,12 +32,15 @@ public class Main {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
+        int totalCase = 1;
         N = Integer.parseInt(br.readLine());
         K = Integer.parseInt(br.readLine());
         dp = new int[N+1][K+1];
-        int totalCase = 1;
-
-        if(N != 1 && K != 1) {
+        for(int i=0; i<N+1; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+    
+        if(N != 1 || K != 1) {
             // 1번을 선택한 경우 + 1번을 선택하지 않은 경우
             totalCase = (findTotalCase(N-3, K-1) + findTotalCase(N-1, K))%MOD;
         }
