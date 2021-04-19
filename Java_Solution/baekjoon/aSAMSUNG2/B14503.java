61ebfba100b96a74b478ec5e43304f739ac0602e
Heon_9u <frtt0608@naver.com>
2021-04-19 21:05:36 +0900

    210419 | 백준|성기출|로봇청소기|40분 소요|BFS로 접근, 바라보는 방향에 따라 이동하는 로직 구현

--- Java_Solution/baekjoon/aSAMSUNG2/B14503.java
+++ Java_Solution/baekjoon/aSAMSUNG2/B14503.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class B14503 {
     static int N, M, cleanCount;
     static int[] dx={-1,0,1,0}, dy={0,1,0,-1};
     static int[][] map;
