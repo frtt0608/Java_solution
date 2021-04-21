b014bf466f631012fecb939e7c6d23f97012080a
Heon_9u <frtt0608@naver.com>
2021-04-21 17:01:22 +0900

    210421 | 백준|삼성기출|35분 소요|배열 범위.. 고려하기

--- Java_Solution/baekjoon/aSAMSUNG2/B15685.java
+++ Java_Solution/baekjoon/aSAMSUNG2/B15685.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class B15685 {
     static final int size = 201;
     static int N, squareCount, x, y;
     static int[] dx={1,0,-1,0}, dy={0,-1,0,1};
