367296819d2744f23d38a1e09d674f192cd8c8ff
Heon_9u <frtt0608@naver.com>
2021-04-21 22:57:30 +0900

    210421 | 백준review|삼성기출|드래곤커브|배열의 범위

--- Java_Solution/baekjoon/Review_Solution/B15685.java
+++ Java_Solution/baekjoon/Review_Solution/B15685.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class B15685 {
     static final int size = 201;
     static int N, x, y, squareCnt;
     static int[] dx={1,0,-1,0}, dy={0,-1,0,1};
