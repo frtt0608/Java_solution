4e6b1b0b058605a0b0d506fb76c5b01addf1fab6
Heon_9u <frtt0608@naver.com>
2021-05-07 10:45:03 +0900

    210507| 백준review|고층빌딩|dp|점화식 세우기|N을 추가하는 것이 아닌, 기존 높이를 1씩 추가한 후, 1을 추가하는 방향으로

--- Java_Solution/baekjoon/Review_Solution/B1328.java
+++ Java_Solution/baekjoon/Review_Solution/B1328.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class B1328 {
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
