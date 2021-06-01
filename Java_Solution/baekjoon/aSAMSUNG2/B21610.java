dc4c2f3241fbc52c9b2b7b6c888826d86f02ee21
Heon_9u <frtt0608@naver.com>
2021-06-01 13:03:47 +0900

    210601 |백준|삼성기출|마법사상어와 비바라기|30분 35초 소요|조건에 따라 단계적으로 구현하기|전역변수 Queue활용

--- Java_Solution/baekjoon/aSAMSUNG2/B21610.java
+++ Java_Solution/baekjoon/aSAMSUNG2/B21610.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class B21610 {
     static int N, M;
     static int[] dx = {0,0,-1,-1,-1,0,1,1,1}, dy = {0,-1,-1,0,1,1,1,0,-1};
     static int[][] waterMap;
